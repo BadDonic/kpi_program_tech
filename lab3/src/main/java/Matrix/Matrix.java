6bad5c244e380b39cf6d601037dade376d1465bd
Danya <dzenik1999@gmail.com>
2018-04-23 15:38:05 +0300

    lab3 with source from lab1

---
+++ lab3/src/main/java/Matrix/Matrix.java
@@ -0,0 +1,119 @@
+package Matrix;
+
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+
+public class Matrix {
+    private int rows;
+    private int cols;
+    private double[][] data;
+
+    public Matrix() { this(1,1); }
+    public Matrix(int size) { this(size, size); }
+
+    public Matrix(int rows, int cols) {
+        if (rows < 1 || cols < 1) throw new InvalidParameterException("Invalid Matrix Dimension");
+        this.rows = rows;
+        this.cols = cols;
+        data = new double[rows][cols];
+        //this(new double[rows][cols]);
+    }
+
+    public Matrix(double[][] data) {
+        if (!isValid(data)) throw new InvalidParameterException("Your array is not a matrix");
+        rows = data.length;
+        cols = data[0].length;
+        this.data = new double[rows][cols];
+        for (int i = 0; i < rows; i++)
+            System.arraycopy(data[i], 0, this.data[i], 0, cols);
+    }
+
+    public double[][] getArray() {
+        return data;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    private boolean isValid (double[][] matrix) {
+        if (matrix == null) throw new NullPointerException("Matrix is null");
+        if (matrix.length == 0) return false;
+        int cols = matrix[0].length;
+        for (int i = 1; i < matrix.length; i++)
+            if (matrix[i].length != cols) return false;
+        return true;
+    }
+
+    public Matrix divide(double k) {
+        if (k == 0) throw new InvalidParameterException("Can not divide on zero");
+        double[][] result = new double[rows][cols];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                result[i][j] = this.data[i][j] / k;
+        return new Matrix(result);
+    }
+
+    public Matrix diff(Matrix matrix) {
+        if (matrix == null) throw new NullPointerException("Can not subtract null matrix");
+        if (matrix.getRows() != rows || matrix.getCols() != cols) throw new InvalidParameterException("Matrices must be of the same dimension");
+        double[][] result = new double[rows][cols];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                result[i][j] = data[i][j] - matrix.getArray()[i][j];
+        return new Matrix(result);
+    }
+
+    @Override
+    public String toString() {
+        return "Matrix{" + "rows=" + rows +
+                ", cols=" + cols +
+                ", data=" + Arrays.deepToString(data) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Matrix matrix = (Matrix) obj;
+        return rows == matrix.rows &&
+                cols == matrix.cols &&
+                Arrays.deepEquals(matrix.getArray(), data);
+    }
+
+    public double determinant() {
+        return determinant(this);
+    }
+
+    public static double determinant(Matrix matrix) {
+        if (matrix == null) throw new NullPointerException("Matrix is null");
+        if (matrix.getCols() != matrix.getRows())
+            throw new InvalidParameterException("Matrix must be square");
+        return determinant(matrix.getArray());
+    }
+
+    private static double determinant(double[][] matrix) {
+        if (matrix.length == 1) return matrix[0][0];
+        if (matrix.length == 2)
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+
+        double det = 0;
+        for (int i = 0; i < matrix[0].length; i++)
+            det += Math.pow(-1, i) * matrix[0][i] * determinant(minor(matrix, i));
+        return det;
+    }
+
+    private static double[][] minor(double[][] matrix, int col) {
+        double[][] minor = new double[matrix.length - 1][matrix.length - 1];
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; i != 0 && j < matrix[i].length; j++)
+                if (j != col)
+                    minor[i - 1][j < col ? j : j - 1] = matrix[i][j];
+        return minor;
+    }
+}
---
+++ lab3/src/test/java/MatrixTest.java
@@ -0,0 +1,140 @@
+import Matrix.Matrix;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.security.InvalidParameterException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixTest {
+    private double[][] test = {
+            {2, 2, 4, 4},
+            {8, 2, 4, 10},
+            {8, 4, 4, 6},
+            {4, 12, 6, 6}
+    };
+
+    @Test @DisplayName("Test constructor without params")
+    void constructorsWithoutParams() {
+        assertArrayEquals(new double[1][1], new Matrix().getArray());
+    }
+
+    @Test @DisplayName("Test constructor with size")
+    void constructorWithSize() {
+        assertAll("constructorWithSize",
+                () -> assertArrayEquals(new double[][]{{0, 0}, {0, 0}}, new Matrix(2).getArray()),
+                () -> assertEquals("Invalid Matrix Dimension", assertThrows(InvalidParameterException.class, () -> new Matrix(0)).getMessage()),
+                () -> assertEquals("Invalid Matrix Dimension",assertThrows(InvalidParameterException.class, () -> new Matrix(-2)).getMessage())
+        );
+    }
+
+    @Test @DisplayName("Test constructor with rows and cols")
+    void constructorWithRowsAndCols() {
+        assertAll("constructorWithRowsAndCols",
+                () -> assertArrayEquals(new double[][]{{0, 0}, {0, 0}, {0, 0}}, new Matrix(3, 2).getArray()),
+                () -> assertEquals("Invalid Matrix Dimension", assertThrows(InvalidParameterException.class, () -> new Matrix(0, 3)).getMessage()),
+                () -> assertEquals("Invalid Matrix Dimension", assertThrows(InvalidParameterException.class, () -> new Matrix(1, -1)).getMessage())
+        );
+    }
+
+    @Test @DisplayName("Test constructor with array")
+    void constructorWithArray() {
+        assertAll("constructorWithArray",
+                () -> assertArrayEquals(test, new Matrix(test).getArray()),
+                () -> assertEquals("Your array is not a matrix", assertThrows(InvalidParameterException.class, () -> new Matrix(new double[][]{{2, 5, 1}, {2}, {3, 2}, {3, 3}})).getMessage()),
+                () -> assertEquals("Matrix is null",assertThrows(NullPointerException.class, () -> new Matrix(null)).getMessage()),
+                () -> assertEquals("Your array is not a matrix",assertThrows(InvalidParameterException.class, () -> new Matrix(new double[][]{})).getMessage())
+        );
+    }
+
+    @Test @DisplayName("Test getArray")
+    void getArray() {
+        assertArrayEquals(test, (new Matrix(test).getArray()));
+    }
+
+    @Test @DisplayName("Test getCols")
+    void getCols() {
+        assertEquals(test[0].length, (new Matrix(test).getCols()));
+    }
+
+    @Test @DisplayName("Test getRows")
+    void getRows() {
+        assertEquals(test.length, (new Matrix(test).getRows()));
+    }
+
+    @Test @DisplayName("Test toString")
+    void ToString() {
+        assertEquals("Matrix{rows=4, cols=4, data=[[2.0, 2.0, 4.0, 4.0]," +
+                " [8.0, 2.0, 4.0, 10.0]," +
+                " [8.0, 4.0, 4.0, 6.0]," +
+                " [4.0, 12.0, 6.0, 6.0]]}", new Matrix(test).toString());
+    }
+
+    @Test @DisplayName("Test equals")
+    void equals() {
+        Matrix same = new Matrix(test);
+        assertAll("equals",
+                () -> assertEquals(same, same),
+                () -> assertNotEquals(null, new Matrix(test)),
+                () -> assertEquals(new Matrix(test), new Matrix(new double[][]{{2, 2, 4, 4}, {8, 2, 4, 10}, {8, 4, 4, 6}, {4, 12, 6, 6}})),
+                () -> assertNotEquals(new Matrix(test), new Matrix())
+        );
+    }
+
+    @Test @DisplayName("Test divide matrix")
+    void divide() {
+        double[][] expectedDivided1 = {
+                {1, 1, 2, 2},
+                {4, 1, 2, 5},
+                {4, 2, 2, 3},
+                {2, 6, 3, 3}
+        };
+        double[][] expectedDivided2 = new double[test.length][test[0].length];
+        double[][] expectedDivided3 = new double[test.length][test[0].length];
+        for (int i = 0; i < test.length; i++)
+            for (int j = 0; j < test[0].length; j++) {
+                expectedDivided2[i][j] = test[i][j] / 3.0;
+                expectedDivided3[i][j] = test[i][j] / -1;
+            }
+
+        assertAll("divide",
+                () -> assertEquals("Can not divide on zero", assertThrows(InvalidParameterException.class, () -> new Matrix().divide(0)).getMessage()),
+                () -> assertEquals(new Matrix(test), new Matrix(test).divide(1)),
+                () -> assertEquals(new Matrix(expectedDivided1), new Matrix(test).divide(2)),
+                () -> assertEquals(new Matrix(expectedDivided2), new Matrix(test).divide(3)),
+                () -> assertEquals(new Matrix(expectedDivided3), new Matrix(test).divide(-1))
+        );
+    }
+
+    @Test @DisplayName("Test diff matrix")
+    void diff() {
+        double[][] expectedRes = {
+                {1, 1, 3, 3},
+                {7, 1, 3, 9},
+                {7, 3, 3, 5},
+                {3, 11, 5, 5}
+        };
+        double[][] arr = {
+                {1, 1, 1, 1},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1}
+        };
+        assertAll("diff",
+                () -> assertEquals("Can not subtract null matrix", assertThrows(NullPointerException.class, () -> new Matrix(test).diff(null)).getMessage()),
+                () -> assertEquals(new Matrix(test.length), new Matrix(test).diff(new Matrix(test))),
+                () -> assertEquals(new Matrix(test), new Matrix(test).diff(new Matrix(test.length))),
+                () -> assertEquals(new Matrix(expectedRes), new Matrix(test).diff(new Matrix(arr))),
+                () -> assertEquals("Matrices must be of the same dimension", assertThrows(InvalidParameterException.class, () -> new Matrix(test).diff(new Matrix())).getMessage())
+        );
+    }
+
+    @Test @DisplayName("Test determinant matrix")
+    void determinant() {
+        assertAll("determinant",
+                () -> assertEquals(0, new Matrix().determinant()),
+                () -> assertEquals(0, Matrix.determinant(new Matrix(4))),
+                () -> assertEquals("Matrix is null", assertThrows(NullPointerException.class, () -> Matrix.determinant(null)).getMessage()),
+                () -> assertEquals("Matrix must be square", assertThrows(InvalidParameterException.class, () -> new Matrix(new double[][]{{0}, {0}}).determinant()).getMessage()),
+                () -> assertEquals(816, new Matrix(test).determinant())
+        );
+    }
+}
